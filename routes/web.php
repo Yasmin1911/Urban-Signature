32bfd14b6758ae7bee76dd143686f296c81015c9
Yasmin <elsy.lemus1911@gmail.com>
2023-05-19 16:24:25 -0600

    Cambios para probar los routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
+});
+Route::get('users/{id}', function ($id) {
+    
 });
 
 Auth::routes();
